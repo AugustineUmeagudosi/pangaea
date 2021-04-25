07002204f3163c3c38344c1e1ac4aeea7aed2160
Austine <augustineumeagudosi@gmail.com>
2021-04-25 21:47:03 +0100

    Updated the readme file

---
+++ seeders/20210425195843-Topic.js
@@ -0,0 +1,13 @@
+const { v4: uuidv4 } = require('uuid');
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.bulkInsert('Topics', [
+      { id: uuidv4(), topic: 'topic1', slug: 'topic1', createdAt: new Date(), updatedAt: new Date() }, 
+      { id: uuidv4(), topic: 'topic2', slug: 'topic2', createdAt: new Date(), updatedAt: new Date() }
+    ]);
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.bulkDelete('Topics', null, {});
+  }
+};
